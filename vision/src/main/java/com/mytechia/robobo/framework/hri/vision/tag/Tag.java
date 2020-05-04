727d3d02dd0278aff142dbc4b4a8a2472f9e0434
Esteban Esquivel <eadan97@gmail.com>
2020-05-04 10:14:00 +0200

    Fix naming on getQuaternion

--- vision/src/main/java/com/mytechia/robobo/framework/hri/vision/tag/Tag.java
+++ vision/src/main/java/com/mytechia/robobo/framework/hri/vision/tag/Tag.java
@@ -117,40 +117,40 @@ public class Tag {
     }
 
     public double[] getQuaternion() {
-        double[] Q = new double[4];
+        double[] q = new double[4];
         Mat rvec = new Mat(1, 3, CvType.CV_64F),
-                R = new Mat(3, 3, CvType.CV_64F);
+                r = new Mat(3, 3, CvType.CV_64F);
         rvec.put(0, 0, rvecs);
-        Calib3d.Rodrigues(rvec, R);
+        Calib3d.Rodrigues(rvec, r);
 
 
-        double trace = R.get(0, 0)[0] +
-                R.get(1, 1)[0] +
-                R.get(2, 2)[0];
+        double trace = r.get(0, 0)[0] +
+                r.get(1, 1)[0] +
+                r.get(2, 2)[0];
 //
         if (trace > 0.0) {
             double s = sqrt(trace + 1.0);
-            Q[3] = (s * 0.5);
+            q[3] = (s * 0.5);
             s = 0.5 / s;
-            Q[0] = ((R.get(2, 1)[0] - R.get(1, 2)[0]) * s);
-            Q[1] = ((R.get(0, 2)[0] - R.get(2, 0)[0]) * s);
-            Q[2] = ((R.get(1, 0)[0] - R.get(0, 1)[0]) * s);
+            q[0] = ((r.get(2, 1)[0] - r.get(1, 2)[0]) * s);
+            q[1] = ((r.get(0, 2)[0] - r.get(2, 0)[0]) * s);
+            q[2] = ((r.get(1, 0)[0] - r.get(0, 1)[0]) * s);
         } else {
-            int i = R.get(0,0)[0] < R.get(1,1)[0] ?
-            (R.get(1,1)[0] < R.get(2,2)[0] ? 2 : 1) :
-            (R.get(0,0)[0] < R.get(2,2)[0] ? 2 : 0);
+            int i = r.get(0,0)[0] < r.get(1,1)[0] ?
+            (r.get(1,1)[0] < r.get(2,2)[0] ? 2 : 1) :
+            (r.get(0,0)[0] < r.get(2,2)[0] ? 2 : 0);
             int j = (i + 1) % 3;
             int k = (i + 2) % 3;
 //
-            double s = sqrt(R.get(i, i)[0] - R.get(j,j)[0] - R.get(k,k)[0] + 1.0);
-            Q[i] = s * 0.5;
+            double s = sqrt(r.get(i, i)[0] - r.get(j,j)[0] - r.get(k,k)[0] + 1.0);
+            q[i] = s * 0.5;
             s = 0.5 / s;
 //
-            Q[3] = (R.get(k,j)[0] - R.get(j,k)[0]) * s;
-            Q[j] = (R.get(j,i)[0] + R.get(i,j)[0]) * s;
-            Q[k] = (R.get(k,i)[0] + R.get(i,k)[0]) * s;
+            q[3] = (r.get(k,j)[0] - r.get(j,k)[0]) * s;
+            q[j] = (r.get(j,i)[0] + r.get(i,j)[0]) * s;
+            q[k] = (r.get(k,i)[0] + r.get(i,k)[0]) * s;
         }
-        return Q;
+        return q;
     }
 
     @Override
