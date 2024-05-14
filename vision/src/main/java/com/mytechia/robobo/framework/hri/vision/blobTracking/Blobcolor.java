1f1bf8e96c394c6e5e5fe0aaaddef877e3766020
rodrigo-mintin <rodrigo.martin@mintforpeople.com>
2024-05-14 13:07:58 +0200

    Upgraded and updated Gradle configs, libraries and such

--- vision/src/main/java/com/mytechia/robobo/framework/hri/vision/blobTracking/Blobcolor.java
+++ vision/src/main/java/com/mytechia/robobo/framework/hri/vision/blobTracking/Blobcolor.java
@@ -127,7 +127,6 @@ public class Blobcolor {
         }
         else {
             ColorCalibrationDataHistogram color = (ColorCalibrationDataHistogram) icolor;
-
             return new Blobcolor(color.getHistMat(),key);
         }
     }
